33e8bfbab1823c494a8a7ccccd6e8633fd9fa1c0
trekkintribe <ttrekkintribe@gmail.com>
2025-06-05 22:52:39 +0530

    Refactor layout and styles; update Prettier config and TypeScript includes. Added AppFooter component and modified globals.css for theme integration.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,87 +1,47 @@
-import Image from 'next/image';
+// import Image from "next/image";
 
 export default function Home() {
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
-        <div className="collapse bg-base-100 border border-base-300">
-          <input type="radio" name="my-accordion-1" defaultChecked />
-          <div className="collapse-title font-semibold">
-            How do I create an account?
+    <div className="grid min-h-screen grid-rows-[20px_1fr_20px] items-center justify-items-center gap-16 p-8 pb-20 font-[family-name:var(--font-geist-sans)] sm:p-20">
+      <main className="row-start-2 flex flex-col items-center gap-[32px] sm:items-start">
+        <div className="drawer">
+          <input id="my-drawer" type="checkbox" className="drawer-toggle" />
+          <div className="drawer-content">
+            {/* Page content here */}
+            <label
+              htmlFor="my-drawer"
+              className="drawer-button btn btn-primary"
+            >
+              Open drawer
+            </label>
           </div>
-          <div className="collapse-content text-sm">
-            Click the "Sign Up" button in the top right corner and follow the
-            registration process.
-          </div>
-        </div>
-        <div className="collapse bg-base-100 border border-base-300">
-          <input type="radio" name="my-accordion-1" />
-          <div className="collapse-title font-semibold">
-            I forgot my password. What should I do?
-          </div>
-          <div className="collapse-content text-sm">
-            Click on "Forgot Password" on the login page and follow the
-            instructions sent to your email.
-          </div>
-        </div>
-        <div className="collapse bg-base-100 border border-base-300">
-          <input type="radio" name="my-accordion-1" />
-          <div className="collapse-title font-semibold">
-            How do I update my profile information?
-          </div>
-          <div className="collapse-content text-sm">
-            Go to "My Account" settings and select "Edit Profile" to make
-            changes.
+          <div className="drawer-side">
+            <label
+              htmlFor="my-drawer"
+              aria-label="close sidebar"
+              className="drawer-overlay"
+            ></label>
+            <div className="relative min-h-full w-80 bg-base-200 p-4">
+              <label
+                htmlFor="my-drawer"
+                aria-label="close sidebar"
+                className="absolute top-0 right-0"
+              >
+                123
+              </label>
+              <ul className="menu text-base-content">
+                {/* Sidebar content here */}
+                <li>
+                  <a>Sidebar Item 1</a>
+                </li>
+                <li>
+                  <a>Sidebar Item 2</a>
+                </li>
+              </ul>
+            </div>
           </div>
         </div>
       </main>
-      <footer className="row-start-3 flex gap-[24px] flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
     </div>
   );
 }
---
+++ src/assets/data/links.module.ts
@@ -0,0 +1,30 @@
+const footerLinks = {
+  company: {
+    title: "company",
+    links: [
+      { id: 1, label: "about", href: "/about-us" },
+      { id: 2, label: "blog", href: "/blog" },
+      { id: 3, label: "destinations", href: "/destinations" },
+      { id: 4, label: "contact", href: "/contact-us" },
+      { id: 5, label: "FAQs", href: "/faq" },
+    ],
+  },
+  legal: {
+    title: "legal",
+    links: [
+      { id: 1, label: "Terms & Conditions", href: "/terms-and-conditions" },
+      { id: 2, label: "privacy policy", href: "/privacy-policy" },
+      { id: 3, label: "Cancellation policy", href: "/cancellation-policy" },
+      { id: 4, label: "Disclaimer", href: "/disclaimer" },
+    ],
+  },
+  quickLinks: {
+    title: "quick links",
+    links: [
+      { id: 1, label: "upcoming destinations", href: "/upcoming-destinations" },
+      { id: 2, label: "our sustainability practices", href: "/" },
+    ],
+  },
+};
+
+export { footerLinks };
